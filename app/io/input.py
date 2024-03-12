a6f02ac039c43c49d4df7d8817c7c1132218510d
verbik <verbikyullya@gmail.com>
2024-03-12 23:59:41 +0200

    Added docstrings

--- app/io/input.py
+++ app/io/input.py
@@ -1,10 +1,24 @@
 def input_text_console():
+    """
+    Input text from the console
+    :return: inputted text from the console
+    """
     pass
 
 
 def input_text_file(file):
+    """
+    Read text from the file using build-in python capabilities
+    :param file: str, path to the file to read text from
+    :return: Read text from the file
+    """
     pass
 
 
 def input_text_pandas(file):
+    """
+    Read text from the file using pandas library
+    :param file: str, path to the file to read text from
+    :return: The text that was read from the file
+    """
     pass
--- app/io/output.py
+++ app/io/output.py
@@ -1,6 +1,16 @@
 def output_text_console(text):
+    """
+    Outputs text to console
+    :param text: str, the text to be outputted to the console
+    :return: None
+    """
     pass
 
 
 def output_text_file(text):
+    """
+    Write text to a file
+    :param text: str, the text to be written to the file
+    :return: None
+    """
     pass
